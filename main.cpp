1783a34b44b29ebd1668bfb5bf09d97d50f1eded
geovanips <geovanips@outlook.com>
2014-08-27 14:58:08 -0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+    char c;
+    cout << "\n Entre com o caractere para mostrar o codigo ASCII:  ";
+    c = getch();
+    cout << "\n" << c << "=== >" << int(c) <<"\n";
+
+
+}
+
+
